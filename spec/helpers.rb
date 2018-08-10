94784a1b7f1a31d3f6ca6973d7a23baac2ea26a6
andrew.howe <andrew.howe@rakuten.com>
2018-08-10 19:00:40 +0900

    move file stuff to lexer spec

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,11 +1,5 @@
 require 'tempfile'
 
 module Helpers
-  def make_test_file(contents)
-    file = Tempfile.new('testfile')
-    file.write contents.join "\n"
-    file.close
-
-    file.path
-  end
+  # pass
 end
--- spec/lexer_spec.rb
+++ spec/lexer_spec.rb
@@ -2,13 +2,27 @@ require './src/lexer.rb'
 require './src/token.rb'
 
 RSpec.describe Lexer do
+  def write_contents(contents)
+    @test_file.open
+    @test_file.write contents.join "\n"
+    @test_file.close
+  end
+
+  before :all do
+    @test_file = Tempfile.new('testfile')
+  end
+
+  after :all do
+    @test_file.delete
+  end
+
   describe '#tokenize' do
     it 'tokenizes variable declarations' do
-      file = make_test_file [
+      write_contents [
         'ほげは 10',
       ]
 
-      tokens = Lexer::tokenize(file).map { |token| [token.type, token.content] }
+      tokens = Lexer::tokenize(@test_file.path).map { |token| [token.type, token.content] }
 
       expect(tokens).to contain_exactly(
         [ Token::ASSIGNMENT, 'ほげ' ],

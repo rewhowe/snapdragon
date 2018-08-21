fcaa95faa55979bc7abd20b60589d985e905cec2
andrew.howe <andrew.howe@rakuten.com>
2018-08-21 17:50:41 +0900

    add test for different types of values

--- spec/lexer_spec.rb
+++ spec/lexer_spec.rb
@@ -29,5 +29,39 @@ RSpec.describe Lexer do
         [ Token::VARIABLE, '10' ],
       )
     end
+
+    it 'recognizes all types of values' do
+      write_contents [
+        '整数は 10',
+        '浮動小数点数は -3.14',
+        '文字列は 「あいうえお」',
+        'ハイレツは 配列',
+        'トルーは 真',
+        'トルーは 肯定',
+        'トルーは はい',
+        'フォルスは 偽',
+        'フォルスは 否定',
+        'フォルスは いいえ',
+        'グローバル変数は それ',
+        'もう一つのグローバル変数は あれ',
+      ]
+
+      tokens = Lexer.tokenize(@test_file.path).map { |token| [ token.type, token.content ] }
+
+      expect(tokens).to contain_exactly(
+        [ Token::ASSIGNMENT, '整数' ],                     [ Token::VARIABLE, '10' ],
+        [ Token::ASSIGNMENT, '浮動小数点数' ],             [ Token::VARIABLE, '-3.14' ],
+        [ Token::ASSIGNMENT, '文字列' ],                   [ Token::VARIABLE, '「あいうえお」' ],
+        [ Token::ASSIGNMENT, 'ハイレツ' ],                 [ Token::VARIABLE, '配列' ],
+        [ Token::ASSIGNMENT, 'トルー' ],                   [ Token::VARIABLE, '真' ],
+        [ Token::ASSIGNMENT, 'トルー' ],                   [ Token::VARIABLE, '肯定' ],
+        [ Token::ASSIGNMENT, 'トルー' ],                   [ Token::VARIABLE, 'はい' ],
+        [ Token::ASSIGNMENT, 'フォルス' ],                 [ Token::VARIABLE, '偽' ],
+        [ Token::ASSIGNMENT, 'フォルス' ],                 [ Token::VARIABLE, '否定' ],
+        [ Token::ASSIGNMENT, 'フォルス' ],                 [ Token::VARIABLE, 'いいえ' ],
+        [ Token::ASSIGNMENT, 'グローバル変数' ],           [ Token::VARIABLE, 'それ' ],
+        [ Token::ASSIGNMENT, 'もう一つのグローバル変数' ], [ Token::VARIABLE, 'あれ' ],
+      )
+    end
   end
 end

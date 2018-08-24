a912ab3f485f116ee554b700a0e29f8ce7bab1b6
andrew.howe <andrew.howe@rakuten.com>
2018-08-24 17:51:20 +0900

    add tokens for if statements

--- src/token.rb
+++ src/token.rb
@@ -15,20 +15,36 @@ class Token
     :ARRAY_BEGIN,    # 配列開き
     :ARRAY_CLOSE,    # 配列閉じ
 
-    # other
-    :SPACE,          # 空白文字
-    :INDENT,         # 全角スペース
+    # variables / functions
     # TODO: make sub types for different types of variables
     :VARIABLE,       # [変数|値]
     :ASSIGNMENT,     # [変数]は
     :PARAMETER,      # [変数|値][助詞]
     :FUNCTION_DEF,   # 関数定義
     :FUNCTION_CALL,  # 関数呼び出し
+
+    # if structure
+    :IF,             # 条件分岐
+    :ELSE_IF,        # 次の条件
+    :ELSE,           # それ以外
+
+    # comparators
+    :COMP_LT,        # A < B
+    :COMP_LTEQ,      # A <= B
+    :COMP_EQ,        # A === B
+    :COMP_NEQ,       # A !== B
+    :COMP_GTEQ,      # A >= B
+    :COM_GT,         # A > B
+
+    # non-code
     :INLINE_COMMENT, # インラインコメント
     :BLOCK_COMMENT,  # ブロックコメント
     :COMMENT,        # コメントテキスト
-    :AND,            # と
     :NO_OP,          # ・・・
+
+    # not used - maybe later?
+    :SPACE,          # 空白文字
+    :INDENT,         # 全角スペース
   ].freeze
   TOKEN_TYPES.each { |constant| const_set(constant, constant.downcase) }
 

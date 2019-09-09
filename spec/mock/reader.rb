992843337aa9476635c3a67b95625c5b189508fe
andrew.howe <andrew.howe@rakuten.com>
2019-09-09 11:33:59 +0900

    create mock for reader

---
+++ spec/mock/reader.rb
@@ -0,0 +1,37 @@
+require './src/tokenizer/reader.rb'
+
+module Mock
+  module Tokenizer
+    class Reader < ::Tokenizer::Reader
+      def initialize(mock_data)
+        @options = {}
+
+        @chunk         = ''
+        @line_num      = 1
+        @output_buffer = []
+        @is_finished   = false
+
+        @mock_data = mock_data.chars
+      end
+
+      def finish
+        @is_finished = true
+      end
+
+      def finished?
+        @is_finished
+      end
+
+      def next_char
+        char = @mock_data.shift
+        @line_num += 1 if char == "\n"
+        char
+      end
+
+      def restore_char(char)
+        @mock_data.unshift char
+        @line_num -= 1 if char == "\n"
+      end
+    end
+  end
+end

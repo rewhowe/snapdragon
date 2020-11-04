f0d81365a0e5234bbe3e5fe2455586b8c0307030
andrew.howe <andrew.howe@rakuten.com>
2020-11-04 11:58:54 +0900

    remove try-catch block and outdent in token printer

--- src/util/token_printer.rb
+++ src/util/token_printer.rb
@@ -3,10 +3,14 @@ module Util
     private_class_method :new
 
     class << self
-      def print_all(lexer)
-        tokens_from(lexer).each do |token|
+      def print_all(lexer, options)
+        tokens_from(lexer, options).each do |token|
           puts [token.type.to_s.blue, token.content.to_s, token.sub_type.to_s.blue].join ' '
         end
+      rescue Tokenizer::Errors::BaseError => e
+        raise e if options[:debug] # show full stack trace if in debug mode
+        puts e.message             # otherwise just display the error message
+        exit
       end
 
       private
@@ -14,15 +18,9 @@ module Util
       def tokens_from(lexer)
         [].tap do |tokens|
           loop do
-            begin
-              token = lexer.next_token
-              break if token.nil?
-              tokens << token
-            rescue Tokenizer::Errors::BaseError => e
-              raise e if options[:debug]
-              puts e.message
-              exit
-            end
+            token = lexer.next_token
+            break if token.nil?
+            tokens << token
           end
         end
       end

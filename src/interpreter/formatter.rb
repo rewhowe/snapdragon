884328be17ae6fa3bf0d70e879dc851a2bc17674
andrew.howe <andrew.howe@rakuten.com>
2020-11-13 12:34:45 +0900

    rubocop for formatter

--- src/interpreter/formatter.rb
+++ src/interpreter/formatter.rb
@@ -3,14 +3,18 @@ module Interpreter
     private_class_method :new
 
     class << self
+      # rubocop:disable Metrics/CyclomaticComplexity
       def output(value)
-        return 'null' if value.nil?
-        return "\"#{value}\"" if value.is_a? String
-        return value.to_i.to_s if value.is_a?(Numeric) && value.to_i == value
-        return "[#{value.map { |v| output v } .join ', '}]" if value.is_a? Array
-        return "{#{value.map { |k, v| "#{k} => #{output v}" } .join ', ' }}" if value.is_a? Hash
-        value.to_s
+        case value
+        when NilClass then 'null'
+        when String   then "\"#{value}\""
+        when Float    then value.to_i == value ? value.to_i.to_s : value.to_s
+        when Array    then "[#{value.map { |v| output v } .join ', '}]"
+        when Hash     then "{#{value.map { |k, v| "#{k} => #{output v}" } .join ', '}}"
+        else value.to_s
+        end
       end
+      # rubocop:enable Metrics/CyclomaticComplexity
     end
   end
 end

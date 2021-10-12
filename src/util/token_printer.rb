904e45427ffaf76a7bf07ece14e26828104617d8
rewhowe <rewhowe@gmail.com>
2021-10-12 11:39:09 +0900

    token printer - print tokens one-by-one

--- src/util/token_printer.rb
+++ src/util/token_printer.rb
@@ -4,20 +4,12 @@ module Util
 
     class << self
       def print_all(lexer)
-        tokens_from(lexer).each do |token|
+        loop do
+          token = lexer.next_token
+
+          break if token.nil?
+
           puts [token.type.to_s.blue, token.content.to_s, token.sub_type.to_s.blue].join ' '
-        end
-      end
-
-      private
-
-      def tokens_from(lexer)
-        [].tap do |tokens|
-          loop do
-            token = lexer.next_token
-            break if token.nil?
-            tokens << token
-          end
         end
       end
     end
